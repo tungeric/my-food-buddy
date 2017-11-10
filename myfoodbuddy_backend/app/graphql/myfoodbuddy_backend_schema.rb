8e04c4138fa1c295e96edc13493a624281eafa79
Eric Tung <eric.p.tung@gmail.com>
2017-11-09 22:24:40 -0800

    Add chowdown model

--- myfoodbuddy_backend/app/graphql/myfoodbuddy_backend_schema.rb
+++
@@ -1,4 +0,0 @@
-MyfoodbuddyBackendSchema = GraphQL::Schema.define do
-  mutation(Types::MutationType)
-  query(Types::QueryType)
-end
--- myfoodbuddy_backend/app/graphql/types/mutation_type.rb
+++
@@ -1,6 +0,0 @@
-Types::MutationType = GraphQL::ObjectType.define do
-  name "Mutation"
-
-  field :createFood, function: Resolvers::CreateFood.new
-
-end
